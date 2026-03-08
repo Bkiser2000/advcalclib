66941b3a1c3bc43a9cf0f0aaadcb19fe8e34d10b
Bkiser2000 <155510865+Bkiser2000@users.noreply.github.com>
2026-03-08 19:16:03 -0400

    Add namespace for calculus functions

--- include/calculus.hpp
+++ include/calculus.hpp
@@ -1,10 +1,12 @@
 #ifndef
 #define CALCULUS_H
 
+namespace calculus {
 double derivative(double (*func)(double), double x, double h);
 
 double integral(double (*func)(double), double a, double b, int n);
 
 double limit(double (*func)(double, double), double x, double y, double h);
+}
 
 #endif
