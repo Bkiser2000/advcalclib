8f558a97292e55fc76de9d701444e243838dd06d
Bkiser2000 <brendan.kiser@yahoo.com>
2026-03-12 19:46:53 -0400

    Fix Homebrew build to only build library, not example

--- Formula/advcalclib.rb
+++ Formula/advcalclib.rb
@@ -1,5 +1,5 @@
 class Advcalclib < Formula
-  desc "Advanced Calculations Library for C++"
+  desc "Advanced Calculator Library for C++"
   homepage "https://github.com/Bkiser2000/advcalclib"
   url "https://github.com/Bkiser2000/advcalclib/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "6ea3e7b7f5783573a51a1efaac009d601416a2a20399f0538d4d4e568568342a"
@@ -11,7 +11,7 @@ class Advcalclib < Formula
     system "cmake", "-B", "build", "-S", ".",
            "-DCMAKE_INSTALL_PREFIX=#{prefix}",
            "-DCMAKE_BUILD_TYPE=Release"
-    system "cmake", "--build", "build"
+    system "cmake", "--build", "build", "--target", "advcalclib"
     system "cmake", "--install", "build"
   end
 end
