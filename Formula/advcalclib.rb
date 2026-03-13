b9aeeeb343ef4f79e59ed289459fdc85829d791d
Bkiser2000 <brendan.kiser@yahoo.com>
2026-03-12 20:06:47 -0400

    Update Homebrew formula for v1.0.1

--- Formula/advcalclib.rb
+++ Formula/advcalclib.rb
@@ -1,8 +1,8 @@
 class Advcalclib < Formula
-  desc "Advanced Calculator Library for C++"
+  desc "Advanced Calculations Library for C++"
   homepage "https://github.com/Bkiser2000/advcalclib"
-  url "https://github.com/Bkiser2000/advcalclib/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "6ea3e7b7f5783573a51a1efaac009d601416a2a20399f0538d4d4e568568342a"
+  url "https://github.com/Bkiser2000/advcalclib/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "0a60ff7bf554ce2c67d19f436643242e6bfcf5529fe3e9d61cad37f0335610c4"
   license "MIT"
   
   depends_on "cmake" => :build
@@ -11,7 +11,7 @@ class Advcalclib < Formula
     system "cmake", "-B", "build", "-S", ".",
            "-DCMAKE_INSTALL_PREFIX=#{prefix}",
            "-DCMAKE_BUILD_TYPE=Release"
-    system "cmake", "--build", "build", "--target", "advcalclib"
+    system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
 end
